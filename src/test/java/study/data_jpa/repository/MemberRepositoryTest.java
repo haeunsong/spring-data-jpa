a418289748ab1666f691b325bc374474275477d7
haeunsong <jamy022576@gmail.com>
2025-01-03 14:12:29 +0900

    @Query 사용하기

--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.data_jpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -15,6 +19,16 @@ class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
 
+    @Test
+    public void testQuery() {
+        Member member1 = new Member("MemberA",10);
+        Member member2 = new Member("MemberB",20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findUser("MemberA",10);
+        assertThat(result.get(0)).isEqualTo(member1);
+    }
     @Test
     public void testMember() {
         Member member = new Member("memberA");
@@ -25,6 +39,33 @@ class MemberRepositoryTest {
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("MemberA");
+        Member member2 = new Member("MemberB");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // 단건 조회 검사
+        Optional<Member> findMember1 = memberRepository.findById(member1.getId());
+        Optional<Member> findMember2 = memberRepository.findById(member2.getId());
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제 검증
+        //삭제 검증 memberJpaRepository.delete(member1); memberJpaRepository.delete(member2);
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 
 }
